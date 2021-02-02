a26cc9e4aa56a1a264d4d7cfb003a7bcb20c7495
JungTag <dbsdudxor121@naver.com>
2021-02-02 16:10:51 +0900

    2/2 do refactoring using function (~27)

--- main.js
+++ main.js
@@ -2,40 +2,70 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const app = http.createServer(function (request, response) {
-  let _url = request.url;
+const templateHTML = (title, list, body) => {
+  return `
+	<!doctype html>
+		<html>
+		<head>
+			<title>WEB1 - ${title}</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<h1><a href="/">WEB</a></h1>
+			${list}
+			${body}
+		</body>
+		</html>
+	`;
+};
+
+const templateList = (files) => {
+  let list = "<ul>";
+  let i = 0;
+  while (i < files.length) {
+    list += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
+    i += 1;
+	}
+	list += "</ul>";
+	return list;
+};
+const app = http.createServer((request, response) => {
+  const _url = request.url;
   const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
   let title = queryData.id;
 
-  if (request.url == "/") {
-    title = "Welcome";
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", (err, files) => {
+        title = "Welcome";
+        desc = "Hello, Node.js";
+				const list = templateList(files);
+        const template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2><p>${desc}</p>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readdir("./data", (err, files) => {
+				const list = templateList(files);
+        fs.readFile(`data/${queryData.id}`, "utf-8", (err, desc) => {
+          const template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2><p>${desc}</p>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
   }
-  if (request.url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, "utf-8", (err, desc) => {
-    const template = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${desc}</p>
-  </body>
-  </html>
-  
-      `;
-    response.end(template);
-  });
 });
 app.listen(3000);
