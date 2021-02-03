564d30f0fc5b92c1ff0bf13f955f0a0d1dbcd246
JungTag <dbsdudxor121@naver.com>
2021-02-03 16:06:03 +0900

    2/3 finish CRUD file (~39)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const templateHTML = (title, list, body) => {
+const templateHTML = (title, list, body, control) => {
   return `
 	<!doctype html>
 		<html>
@@ -13,7 +13,7 @@ const templateHTML = (title, list, body) => {
 		<body>
 			<h1><a href="/">WEB</a></h1>
       ${list}
-      <a href="/create">create</a>
+      ${control}
 			${body}
 		</body>
 		</html>
@@ -45,7 +45,8 @@ const app = http.createServer((request, response) => {
         const template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2><p>${desc}</p>`
+          `<h2>${title}</h2><p>${desc}</p>`,
+          `<a href="/create">create</a>`
         );
         response.writeHead(200);
         response.end(template);
@@ -58,7 +59,14 @@ const app = http.createServer((request, response) => {
           const template = templateHTML(
             title,
             list,
-            `<h2>${title}</h2><p>${desc}</p>`
+            `<h2>${title}</h2><p>${desc}</p>`,`
+            <a href="/create">create</a>
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>
+            `
           );
           response.writeHead(200);
           response.end(template);
@@ -70,16 +78,14 @@ const app = http.createServer((request, response) => {
       title = "WEB = create";
       const list = templateList(files);
       const template = templateHTML(title, list, `
-        <form action="http://localhost:3000/create_process" method="post">
-  <p><input type="text" name="title" placeholder="title"></p>
-  <p>
-    <textarea name="description" placeholder="description"></textarea>
-  </p>
-  <p>
-    <input type="submit">
-  </p>
-</form>
-        `);
+        <form action="/create_process" method="post">
+         <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+          <textarea name="description" placeholder="description"></textarea>
+          </p>
+        <p><input type="submit"></p>
+        </form> 
+        `, '');
       response.writeHead(200);
       response.end(template);
     });
@@ -100,6 +106,68 @@ const app = http.createServer((request, response) => {
           response.end();
         }
       });
+    });
+  } else if(pathname === '/update') {    
+    fs.readdir("./data", (err, files) => {
+      title = queryData.id;
+      const list = templateList(files);
+      fs.readFile(`data/${queryData.id}`, "utf-8", (err, desc) => {
+        const template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+           <p>
+           <textarea name="description" placeholder="description">${desc}</textarea>
+           </p>
+          <p><input type="submit"></p>
+          </form> 
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if(pathname === '/update_process') {
+    let body = '';
+    request.on('data', (data) => {
+      body += data;
+    });
+    request.on('end', () => {
+      const post = qs.parse(body); // 정보를 객체화시킴
+      const id = post.id;
+      const title = post.title;
+      const description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, err => {
+        if (err) 
+          return console.log(err);
+        else {
+          fs.writeFile(`data/${title}`, description, 'utf-8', err => {
+            if (err) 
+              return console.log(err);
+            else {
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            }
+          });
+        }
+      });
+    });
+  } else if(pathname === '/delete_process') {
+    let body = '';
+    request.on('data', (data) => {
+      body += data;
+    });
+    request.on('end', () => {
+      const post = qs.parse(body); // 정보를 객체화시킴
+      const id = post.id;
+      fs.unlink(`data/${id}`, () => {
+        response.writeHead(302, {Location: `/`});
+        response.end();
+      })
     });
   }
   else {
