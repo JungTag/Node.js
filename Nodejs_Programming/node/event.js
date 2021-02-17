99d14716f09fb93bb378d1793d0d01a5f2e150db
JungTag <dbsdudxor121@naver.com>
2021-02-18 00:41:42 +0900

    2/18 Event

---
+++ Nodejs_Programming/node/event.js
@@ -0,0 +1,24 @@
+// process.on('exit', () => {
+//     console.log('exit 이벤트 발생');
+// });
+
+// console.log('3초 후 프로세스 종료');
+
+// setTimeout(() => {
+//     process.exit();
+// }, 3000);
+
+
+
+
+
+
+process.on('custom', (param) => {
+    console.log(`custom 이벤트 발생 (param: ${param})`);
+});
+
+console.log('3초 후 custom 이벤트 발생');
+
+setTimeout(() => {
+    process.emit('custom', 'Hello World');
+}, 3000);
